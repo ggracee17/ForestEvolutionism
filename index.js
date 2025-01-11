23f7886b8ef93c8cb716af4f934e1e69a51d53ee
Grace Shen <106719734+ggracee17@users.noreply.github.com>
2025-01-12 00:34:30 +1100

    fix health calculation

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function showWinner() {
     var round = document.getElementById("round").value;
     var player_number = document.getElementById("no_players").value;
     var round_health = 0;
-    console.log(round, player_number, first_player_name, first_player_health, second_player_name, second_player_health, card1, card2, letter1, letter2, suit1, suit2, health1, health2);
+    console.log(round, round_health, player_number, first_player_name, first_player_health, second_player_name, second_player_health, card1, card2, letter1, letter2, suit1, suit2, health1, health2);
     
     if (letter1 == letter2) {
         if (player_number >= 12) {
@@ -145,15 +145,16 @@ function showWinner() {
         document.getElementById("winner").innerHTML = 'Please check that player identities are entered correctly';
     } else {
         if (winner == first_player) {
-            document.getElementById(first_player_health).value += round_health;
-            document.getElementById(second_player_health).value -= round_health;
+            health1 = health1 + round_health;
+            health2 = health2 - round_health;
             document.getElementById("winner").innerHTML = 'Successful, player ' + winner + ' wins';
         } else {
-            document.getElementById(first_player_health).value -= round_health;
-            document.getElementById(second_player_health).value += round_health;
+            health1 = health1 - round_health;
+            health2 = health2 + round_health;
             document.getElementById("winner").innerHTML = 'Failed, player ' + winner + ' wins';
         }
     }
-    
+    document.getElementById(first_player_health).value = health1;
+    document.getElementById(second_player_health).value = health2;
 }
 
